ab3370121c437e45454e56c6502a5bbebd2cae38
Tran Vuong Quang <hiimkol@gmail.com>
2018-08-15 17:08:14 +0700

    update project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Recipe } from './../recipe';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simple test',
+      'https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
+    new Recipe('A Test Recipe', 'This is simple test',
+      'https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
+    new Recipe('A Test Recipe', 'This is simple test',
+      'https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
